38945793ea6da09422e19bdaf6f84e448c3d024f
Liberty <liberty.tamayo@escuelaestech.es>
2024-08-07 17:46:07 +0200

    firebase implementado

---
+++ faemood/src/config/firebase.jsx
@@ -0,0 +1,9 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
